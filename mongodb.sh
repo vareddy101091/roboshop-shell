64bc9559c95235a280dc18dfb6a2e5dbac626e02
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-04-18 17:01:58 +0530

    MongoDb Script

--- mongodb.sh
+++ mongodb.sh
@@ -5,11 +5,14 @@ echo -e "\e[32m>>>>>>>>> Setup the MongoDB repo file <<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[32m>>>>>>>>>  Install MongoDB  <<<<<<<<\e[0m"
+
 yum install mongodb-org -y
 
 echo -e "\e[32m>>>>>>>>>  Update Listen Address  <<<<<<<<\e[0m"
+
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 
-echo -e "\e[32m>>>>>>>>>  Enable and Restart Service  <<<<<<<<\e[0m"
+echo -e "\e[32m>>>>>>>>>  Enable and Restart Service  <<<<<<<<\e[0m
+
 systemctl enable mongod
 systemctl restart mongod
