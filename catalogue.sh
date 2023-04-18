c83961cd46eaaaf70803ecacb9d67126d07ebb2d
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-04-18 22:46:51 +0530

    Catalogue Script

--- catalogue.sh
+++ catalogue.sh
@@ -1,25 +1,19 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
 yum install nodejs -y
-
 useradd roboshop
-rm -rf /app
 mkdir /app
-
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
-
-cd /app
 npm install
 
-cp /catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service
 
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
 
-cp /mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org-shell -y
 
