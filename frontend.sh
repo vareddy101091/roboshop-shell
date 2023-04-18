644f5eb344368b7afe487b572bcb6131e378afb5
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-04-18 17:10:20 +0530

    Catalogue Script

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ echo -e "\e[32m>>>>>>>>> Install nginx <<<<<<<<\e[0m"
 yum install nginx -y
 
 echo -e "\e[32m>>>>>>>>> Create Nginx Reverse Proxy Configuration. <<<<<<<<\e[0m"
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${script_path}roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[32m>>>>>>>>> Remove Default Content Nginx is serving <<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
