8fc01d2a44a0800c5fcea031b0257e6e019a13f9
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-04-22 04:10:13 +0530

    Dispatch Script

--- common.sh
+++ common.sh
@@ -2,3 +2,35 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
+func_nodejs() {
+  echo -e "\e[34m>>>>>>>>> Configuring NodeJS repos <<<<<<<<\e[0m"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+  echo -e "\e[34m>>>>>>>>> Install NodeJS <<<<<<<<\e[0m"
+  yum install nodejs -y
+
+  echo -e "\e[34m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
+  useradd ${app_user}
+
+  echo -e "\e[34m>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
+  rm -rf /app
+  mkdir /app
+
+  echo -e "\e[34m>>>>>>>>> Download App Content <<<<<<<<\e[0m"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  cd /app
+
+  echo -e "\e[34m>>>>>>>>> Unzip App Content <<<<<<<<\e[0m"
+  unzip /tmp/${component}.zip
+
+  echo -e "\e[34m>>>>>>>>> Install NodeJS Dependencies <<<<<<<<\e[0m"
+  npm install
+
+  echo -e "\e[34m>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
+  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+
+  echo -e "\e[34m>>>>>>>>> Start Cart Service <<<<<<<<\e[0m"
+  systemctl daemon-reload
+  systemctl enable ${component}
+  systemctl restart ${component}
+}
--- user.sh
+++ user.sh
@@ -2,37 +2,9 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
+component=user
 
-echo -e "\e[33m>>>>>>>>> Configuring NodeJS repos <<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
-echo -e "\e[33m>>>>>>>>> Install NodeJS <<<<<<<<\e[0m"
-yum install nodejs -y
-
-echo -e "\e[33m>>>>>>>>> Add Application User <<<<<<<<\e[0m"
-useradd ${app_user}
-
-echo -e "\e[33m>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[33m>>>>>>>>> Download App Content <<<<<<<<\e[0m"
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-
-echo -e "\e[33m>>>>>>>>> Unzip App Content <<<<<<<<\e[0m"
-unzip /tmp/user.zip
-
-echo -e "\e[33m>>>>>>>>> Install NodeJS Dependencies <<<<<<<<\e[0m"
-npm install
-
-echo -e "\e[33m>>>>>>>>> Create Application Directory <<<<<<<<\e[0m"
-cp ${script_path}/user.service /etc/systemd/system/user.service
-
-echo -e "\e[33m>>>>>>>>> Start User Service <<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+func_nodejs
 
 echo -e "\e[33m>>>>>>>>> Copy MongoDB repo <<<<<<<<\e[0m"
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
