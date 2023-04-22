637293f71034f863527bb3da6c02e3cc87aafe10
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-04-22 17:08:07 +0530

    Function Add Script

--- common.sh
+++ common.sh
@@ -2,55 +2,86 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
-print_head() {
+func_print_head() {
   echo -e "\e[31m>>>>>>>>> $1 <<<<<<<<\e[0m"
   }
 
-schema_setup() {
+func_schema_setup() {
   if [ "$schema_setup" == "mongo" ]; then
-  print_head "Copy MongoDB repo"
+  func_print_head "Copy MongoDB repo"
   cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
-  print_head "Install MongoDB Client"
+  func_print_head "Install MongoDB Client"
   yum install mongodb-org-shell -y
 
-  print_head "Load Schema"
+  func_print_head "Load Schema"
   mongo --host mongodb-dev.vardevops.online </app/schema/catalogue.js
+  fi
+  if [ "${schema_setup}" == "mysql" ]; then
+   func_print_head "Install MySQL Client"
+   yum install mysql -y
+
+   func_print_head "Load Schema"
+   mysql -h mysql-dev.vardevops.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
   fi
 }
 
-
-func_nodejs() {
-  print_head "Configuring NodeJS repos"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
-  print_head "Install NodeJS"
-  yum install nodejs -y
-
-  print_head "Add Application User"
+func_app_prereq() {
+  func_print_head "Create Application User"
   useradd ${app_user}
 
-  print_head "Create Application Directory"
+  func_print_head "Create Application Directory"
   rm -rf /app
   mkdir /app
 
-  print_head "Download App Content"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+  func_print_head "Download Application Content"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+
+
+  func_print_head "Extract Application Content"
   cd /app
-
-  print_head "Unzip App Content"
   unzip /tmp/${component}.zip
+}
 
-  print_head "Install NodeJS Dependencies"
+func_systemd_setup() {
+  func_print_head "Create Application Directory"
+    cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+
+    func_print_head "Start Cart Service"
+    systemctl daemon-reload
+    systemctl enable ${component}
+    systemctl restart ${component}
+
+}
+
+func_nodejs() {
+  func_print_head "Configuring NodeJS repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+  func_print_head "Install NodeJS"
+  yum install nodejs -y
+
+  func_app_prereq
+
+  func_print_head "Install NodeJS Dependencies"
   npm install
 
-  print_head "Create Application Directory"
-  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+  func_schema_setup
+  func_systemd_setup
+}
 
-  print_head "Start Cart Service"
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
+func_java() {
+  func_print_head "Install Maven"
+  yum install maven -y
 
-  schema_setup
+  func_app_prereq
+
+  func_print_head "Download Maven Dependencies"
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
+
+
+
+  func_schema_setup
+  func_systemd_setup
 }
--- shipping.sh
+++ shipping.sh
@@ -8,38 +8,7 @@ if [ -z "$mysql_root_password" ]; then
   exit
 fi
 
-echo -e "\e[31m>>>>>>>>> Install Maven <<<<<<<<\e[0m"
-yum install maven -y
+component="shipping"
+schema_setup=mysql
+func_java()
 
-echo -e "\e[31m>>>>>>>>> Create App User <<<<<<<<\e[0m"
-useradd ${app_user}
-
-echo -e "\e[31m>>>>>>>>> Create App Directory <<<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[31m>>>>>>>>> Download App Content <<<<<<<<\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-
-echo -e "\e[31m>>>>>>>>> Extract App Content <<<<<<<<\e[0m"
-cd /app
-unzip /tmp/shipping.zip
-
-echo -e "\e[31m>>>>>>>>> Download Maven Dependencies <<<<<<<<\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-echo -e "\e[31m>>>>>>>>> Install MySQL <<<<<<<<\e[0m"
-yum install mysql -y
-
-echo -e "\e[31m>>>>>>>>> Load Schema <<<<<<<<\e[0m"
-mysql -h mysql-dev.vardevops.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-
-echo -e "\e[31m>>>>>>>>> Setup SystemD Service <<<<<<<<\e[0m"
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-
-
-echo -e "\e[31m>>>>>>>>> Start Shipping Service <<<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
