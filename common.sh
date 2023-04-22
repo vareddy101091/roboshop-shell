d78217e862cb5f653118dc0076818f4f40077c3c
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-04-22 18:44:46 +0530

    Function Add Script

--- common.sh
+++ common.sh
@@ -5,6 +5,7 @@ log_file=/tmp/roboshop.log
 
 func_print_head() {
   echo -e "\e[31m>>>>>>>>> $1 <<<<<<<<\e[0m"
+  echo -e "\e[31m>>>>>>>>> $1 <<<<<<<<\e[0m" &>>log_file
   }
 func_stat_check() {
    if [ $1 -eq 0 ]; then
