536748302404aec9b9bc859aeab700426b11911b
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-04-23 04:17:12 +0530

    Function Add Script

--- mongodb.sh
+++ mongodb.sh
@@ -2,22 +2,22 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
+func_print_head "Setup the MongoDB repo file"
+cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+func_stat_check $?
 
-echo -e "\e[32m>>>>>>>>> Setup the MongoDB repo file <<<<<<<<\e[0m"
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+func_print_head "Install MongoDB"
+yum install mongodb-org -y &>>$log_file
+func_stat_check $?
 
-echo -e "\e[32m>>>>>>>>>  Install MongoDB  <<<<<<<<\e[0m"
-
-yum install mongodb-org -y
-
-echo -e "\e[32m>>>>>>>>>  Update Listen Address  <<<<<<<<\e[0m"
-
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+func_print_head  "Update Listen Address"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf &>>$log_file
+func_stat_check $?
 
 
-echo -e "\e[32m>>>>>>>>>  Enable and Restart Service  <<<<<<<<\e[0m"
-
-systemctl enable mongod
-systemctl restart mongod
+func_print_head "Enable and Restart Service"
+systemctl enable mongod &>>$log_file
+systemctl restart mongod &>>$log_file
 netstat -lntp
+func_stat_check $?
 
