8152da085bc7363cffb97c004ec598d1b3f5e355
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-04-23 02:14:21 +0530

    Function Add Script

--- frontend.sh
+++ frontend.sh
@@ -3,26 +3,32 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 
+func_print_head "Install nginx"
+yum install nginx -y &>>$log_file
+func_stat_check $?
 
-echo -e "\e[32m>>>>>>>>> Install nginx <<<<<<<<\e[0m"
-yum install nginx -y
+func_print_head "Copy Roboshop Config File."
+cp ${script_path}/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
+func_stat_check $?
 
-echo -e "\e[32m>>>>>>>>> Create Nginx Reverse Proxy Configuration. <<<<<<<<\e[0m"
-cp ${script_path}/roboshop.conf /etc/nginx/default.d/roboshop.conf
+func_print_head "Clean Old App Content"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+func_stat_check $?
 
-echo -e "\e[32m>>>>>>>>> Remove Default Content Nginx is serving <<<<<<<<\e[0m"
-rm -rf /usr/share/nginx/html/*
+func_print_head "Download App Content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+func_stat_check $?
 
-echo -e "\e[32m>>>>>>>>> Download the frontend content <<<<<<<<\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+func_print_head "Extracting App content"
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
+func_stat_check $?
 
-echo -e "\e[32m>>>>>>>>> Extract the frontend content. <<<<<<<<\e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+func_print_head "Start & Enable Nginx service"
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
+func_stat_check $?
 
-echo -e "\e[32m>>>>>>>>>Start & Enable Nginx service  <<<<<<<<\e[0m"
-systemctl restart nginx
-systemctl enable nginx
 
 
 
