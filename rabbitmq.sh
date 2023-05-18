99347f4991758fc9d87e465d8b06d4099e86648d
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-18 18:01:32 +0530

    Ansible

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -7,6 +7,10 @@ if [ -z "$rabbitmq_appuser_password" ]; then
   echo Input Roboshop Appuser Password Missing
   exit 1
 fi
+func_print_head "Install ErLang & RabbitMQ"
+yum install erlang rabbitmq-server -y &>>$log_file
+func_stat_check $?
+
 
 func_print_head "Setup ErLang Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$log_file
@@ -16,9 +20,6 @@ func_print_head "Setup RabbitMQ Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log_file
 func_stat_check $?
 
-func_print_head "Install ErLang & RabbitMQ"
-yum install erlang rabbitmq-server -y &>>$log_file
-func_stat_check $?
 
 func_print_head "Start RabbitMQ Service"
 systemctl enable rabbitmq-server &>>$log_file
